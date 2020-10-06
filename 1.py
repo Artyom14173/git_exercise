a8bd3af3cc9ca9194dfb6d59d2356de0f668fe89
Artyom <Artsiom2627@gmail.com>
2020-10-06 23:58:13 +0300

    DZ1

---
+++ 1.py
@@ -0,0 +1,30 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Silence is golden")
+Silence is golden
+>>> a = 45
+>>> b = 7
+>>> print ("a + b =", a + b)
+a + b = 52
+>>> print ("a * b =", a * b)
+a * b = 315
+>>> a = 3
+>>> b = 10
+>>> c = 57
+>>> print ("a * 2 =", a * 2)
+a * 2 = 6
+>>> print ("b - 3 =", b - 3)
+b - 3 = 7
+>>> print ("c * c =", c * c)
+c * c = 3249
+>>> print ("6 + 7 + 3249 =", 6 + 7 + 3249)
+6 + 7 + 3249 = 3262
+>>> '''
+AAAAAAAA
+AAAAAAAA
+AAAAAAAA
+AAAAAAAA
+AAAAAAAA
+'''
+'\nAAAAAAAA\nAAAAAAAA\nAAAAAAAA\nAAAAAAAA\nAAAAAAAA\n'
+>>> 
