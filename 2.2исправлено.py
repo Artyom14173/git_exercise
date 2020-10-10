4e57b866577ac771455e196c4fdaa17826ca0b6b
Artyom14173 <72384160+Artyom14173@users.noreply.github.com>
2020-10-10 11:27:47 +0300

    Add files via upload

---
+++ 2.2исправлено.py
@@ -0,0 +1,27 @@
+x = int(input('Введите номер месяца'))
+if x = 1:
+    print(январь)
+elif x = 2:
+    print(февраль)
+elif x = 3:
+    print(март)
+elif x = 4:
+    print(апрель)
+elif x = 5:
+    print(май)
+elif x = 6:
+    print(июнь)
+elif x = 7:
+    print(июль)
+elif x = 8:
+    print(август)
+elif x = 9:
+    print(сентябрь)
+elif x = 10:
+    print(октябрь)
+elif x = 11:
+    print(ноябрь)
+elif x = 12:
+    print(декабрь)
+else:
+    print(ошибка 666)
