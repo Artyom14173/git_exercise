03a4d98908f8f7c11a801d92ade786104c29d74c
Artyom14173 <72384160+Artyom14173@users.noreply.github.com>
2020-10-23 22:48:22 +0300

    4 дз

---
+++ 4абвг.py
@@ -0,0 +1,108 @@
+#1-ое задание
+kolvo_km_v_1day = 10
+#вводим колво км в 1 день
+kolvo_km_v_2day = kolvo_km_v_1day + (kolvo_km_v_1day * 0,1)
+#высчитываем колво км во 2 день
+print(kolvo_km_v_2day)
+#выводим колво км во 2 день
+kolvo_km_v_3day = kolvo_km_v_2day + (kolvo_km_v_2day * 0,1)
+#высчитываем колво км в 3 день
+print(kolvo_km_v_3day)
+#выводим колво км в 3 день
+kolvo_km_v_4day = kolvo_km_v_3day + (kolvo_km_v_3day * 0,1) 
+#высчитываем колво км в 4 день
+print(kolvo_km_v_4day)
+#выводим колво км в 4 день
+kolvo_km_v_5day = kolvo_km_v_4day + (kolvo_km_v_4day * 0,1)
+#высчитываем колво км в 5 день
+print(kolvo_km_v_5day)
+#выводим колво км в 5 день
+kolvo_km_v_6day = kolvo_km_v_5day + (kolvo_km_v_5day * 0,1)
+#высчитываем колво км в 6 день
+print(kolvo_km_v_6day)
+#выводим колво км в 6 день
+kolvo_km_v_7day = kolvo_km_v_6day + (kolvo_km_v_6day * 0,1)
+#высчитываем колво км в 7 день
+print(kolvo_km_v_7day)
+#выводим колво км в 7 день
+kolvo_km_v_8day = kolvo_km_v_7day + (kolvo_km_v_7day * 0,1)
+#высчитываем колво км в 8 день
+print(kolvo_km_v_8day)
+#выводим колво км в 8 день
+kolvo_km_v_9day = kolvo_km_v_8day + (kolvo_km_v_8day * 0,1)
+#высчитываем колво км в 9 день
+print(kolvo_km_v_9day)
+#выводим колво км в 9 день
+kolvo_km_v_10day = kolvo_km_v_9day + (kolvo_km_v_9day * 0,1)
+#высчитываем колво км в 10 день
+print(kolvo_km_v_10day)
+#выводим колво км в 10 день
+#решили 1-ое задание
+#2-ое задание
+summarniy_put_za_7days = kolvo_km_v_1day + kolvo_km_v_2day + kolvo_km_v_3day + kolvo_km_v_4day + kolvo_km_v_5day + kolvo_km_v_6day + kolvo_km_v_7day
+#высчитывем суммарный путь за 7 дней
+print(summarniy_put_za_7days)
+#выводим суммарный путь за 7 дней
+#решили 2-ое задание
+#3-е задание
+if summarniy_put_za_ndays = kolvo_km_v_1day:
+#если сум путь за н дней равно колво км в 1 день то
+    print(summarniy_put_za_ndays)
+#вывести суммарный путь за н дней
+elif summarniy_put_za_ndays = kolvo_km_v_1day + kolvo_km_v_2day:
+#если сум путь за н дней равно сумме колво км в 1 и во 2 день то
+    print(summarniy_put_za_ndays)
+#вывести суммарный путь за н дней
+elif summarniy_put_za_ndays = kolvo_km_v_1day + kolvo_km_v_2day +kolvo_km_v_3day:
+#если сум путь за н дней равно сумме колво км в 1 день 2 и 3 то
+    print(summarniy_put_za_ndays)
+#вывести суммарный путь за н дней
+elif summarniy_put_za_ndays = kolvo_km_v_1day + kolvo_km_v_2day + kolvo_km_v_3day + kolvo_km_v_4day:
+#если сум путь за н дней равно сумме колво км в 1 день 2 3 и 4 то
+    print(summarniy_put_za_ndays)
+#вывести суммарный путь за н дней
+elif summarniy_put_za_ndays = kolvo_km_v_1day + kolvo_km_v_2day + kolvo_km_v_3day + kolvo_km_v_4day + kolvo_km_v_5day:
+#если сум путь за н дней равно сумме колво км в 1 день 2 3 4 и 5 то
+    print(summarniy_put_za_ndays)
+#вывести суммарный путь за н дней
+elif summarniy_put_za_ndays = kolvo_km_v_1day + kolvo_km_v_2day + kolvo_km_v_3day + kolvo_km_v_4day + kolvo_km_v_5day + kolvo_km_v_6day:
+#если сум путь за н дней равно сумме колво км в 1 день 2 3 4 5 и 6 то
+    print(summarniy_put_za_ndays)
+#вывести суммарный путь за н дней
+elif summarniy_put_za_ndays = kolvo_km_v_1day + kolvo_km_v_2day + kolvo_km_v_3day + kolvo_km_v_4day + kolvo_km_v_5day + kolvo_km_v_6day + kolvo_km_v_7day:
+#если сум путь за н дней равно сумме колво км в 1 день 2 3 4 5 6 и 7 то
+    print(summarniy_put_za_ndays)
+#вывести суммарный путь за н дней
+elif summarniy_put_za_ndays = kolvo_km_v_1day + kolvo_km_v_2day + kolvo_km_v_3day + kolvo_km_v_4day + kolvo_km_v_5day + kolvo_km_v_6day + kolvo_km_v_7day + kolvo_km_v_8day:
+#если сум путь за н дней равно сумме колво км в 1 день 2 3 4 5 6 7 и 8 то
+    print(summarniy_put_za_ndays)
+#вывести суммарный путь за н дней
+elif summarniy_put_za_ndays = kolvo_km_v_1day + kolvo_km_v_2day + kolvo_km_v_3day + kolvo_km_v_4day + kolvo_km_v_5day + kolvo_km_v_6day + kolvo_km_v_7day + kolvo_km_v_8day + kolvo_km_v_9day:
+#если сум путь за н дней равно сумме колво км в 1 день 2 3 4 5 6 7 8 и 9 то
+    print(summarniy_put_za_ndays)
+#вывести суммарный путь за н дней
+elif summarniy_put_za_ndays = kolvo_km_v_1day + kolvo_km_v_2day + kolvo_km_v_3day + kolvo_km_v_4day + kolvo_km_v_5day + kolvo_km_v_6day + kolvo_km_v_7day + kolvo_km_v_8day + kolvo_km_v_9day + kolvo_km_v_10day:
+#если сум путь за н дней равно сумме колво км в 1 день 2 3 4 5 6 7 8 9 и 10 то
+    print(summarniy_put_za_ndays)
+#вывести суммарный путь за н дней
+else:
+#иначе
+    print(error)
+#вывести: ошибка
+#закончили 3-е задание
+#4-ое задание
+#вообще не понимаю как это делать
+kolvo_km_za_2days = 21
+#колво км за 2 дня
+kolvo_km_za_3days = 33
+#колво км за 3 дня
+kolvo_km_za_4days = 46
+#колво км за 4 дня
+kolvo_km_za_5days = 60
+#колво км за 5 дней
+kolvo_km_za_6days = 75
+#колво км за 6 дней
+kolvo_km_za_7days = 92
+#колво км за 7 дней
+#закончили 4-ое задание
+                
